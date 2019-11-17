f4e26ecc9a05676cf6b1c84473a9c82808b8c9c7
tchoe4jc <39310629+tchoe4jc@users.noreply.github.com>
2019-11-16 19:12:25 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,95 @@
+const express         =     require('express')
+  , passport          =     require('passport')
+  , FacebookStrategy  =     require('passport-facebook').Strategy
+  , session           =     require('express-session')
+  , cookieParser      =     require('cookie-parser')
+  , bodyParser        =     require('body-parser')
+  , config            =     require('./configuration/config')
+  , mysql             =     require('mysql')
+  , app               =     express();
+
+//Define MySQL parameter in Config.js file.
+const pool = mysql.createPool({
+  host     : config.host,
+  user     : config.username,
+  password : config.password,
+  database : config.database
+});
+
+// Passport session setup.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the FacebookStrategy within Passport.
+
+passport.use(new FacebookStrategy({
+    clientID: config.facebook_api_key,
+    clientSecret:config.facebook_api_secret ,
+    callbackURL: config.callback_url
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      //Check whether the User exists or not using profile.id
+      if(config.use_database) {
+        // if sets to true
+        pool.query("SELECT * from user_info where user_id="+profile.id, (err,rows) => {
+          if(err) throw err;
+          if(rows && rows.length === 0) {
+              console.log("There is no such user, adding now");
+              pool.query("INSERT into user_info(user_id,user_name) VALUES('"+profile.id+"','"+profile.username+"')");
+          } else {
+              console.log("User already exists in database");
+          }
+        });
+      }
+      return done(null, profile);
+    });
+  }
+));
+var engine = require('consolidate')
+
+app.set('views', __dirname + '/views');
+//app.engine('html', engine.mustache)
+app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: 'keyboard cat', key: 'sid'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/auth/facebook', passport.authenticate('facebook',{scope:'email'}));
+
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect : '/', failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
+
+app.listen(3000);
---
+++ configuration/config.js
@@ -0,0 +1,10 @@
+module.exports={
+  "facebook_api_key"      :     "758035714706708",
+  "facebook_api_secret"   :     "f9b1fb2f1910bbaa846ac6759e38076e",
+  "callback_url"          :     "http://localhost:3000/auth/facebook/callback",
+  "use_database"          :      false,
+  "host"                  :     "localhost",
+  "username"              :     "root",
+  "password"              :     "",
+  "database"              :     "Database Name"
+}
